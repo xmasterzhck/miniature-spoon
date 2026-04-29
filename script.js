fa1f287fed902db60733248d15cd12dec35a8b52
xmasterzhck <152147246+xmasterzhck@users.noreply.github.com>
2026-04-30 07:56:44 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function goToProfile() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("browserPopup").style.display = "flex";
+}
+
+function closeBrowser() {
+    document.getElementById("browserPopup").style.display = "none";
+}
